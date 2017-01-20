6d21723473f05855ba18973938fd86d3a99051bd
Esteban Sotillo <esteban.sotillo@cpnv.ch>
2017-01-20 14:39:09 +0100

    Ready to use but in test

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-VerifJS
+# VerifJS
+
+## Getting stated
+
+1. include the script on the html `<script src="verif.js" ></script>`
+2. add a `data-verif` attribute to your inputs
+```html
+<input type="text" data-verif="required|text|min_l:6|max_l:20">
+```
+3. add some css for the error message
+```css
+.verif_error{
+  border-bottom: 1px solid #bb3454 !important;
+}
+.verif_message_error{
+  color:#bb3454;
+}
+```
+
+## Rules
+
+- `required` : value != null
+- `int` : value is an integer
+- `double` : value is a double (or float)
+- `text` : value is [a-zA-Z0-9_ ]* with space
+- `alphanumerique` : value is [a-zA-Z0-9_]* without space
+- `email` : value is an email
+- `date` : value is like 'dd.mm.yyyy'
+- `time` : value is like 'HH:MM'
+- `date_time` : value is like 'dd.mm.yyyy HH:MM'
+- `max:<arg>` : value is <= than arg
+- `min:<arg>` : value is >= than arg
+- `max_l:<arg>` : value.length is <= than arg
+- `min_l:<arg>` : value.length is >= than arg
+- `date_past` : date is before now
+- `date_futur` : date is after now
+- `date_more_diff:<arg>` : difference between value and now is more or equal than arg (in month)
+- `date_more_diff:<arg>` : difference between value and now is less than arg (in month)
+
+## Add a custom rule
+
+in an other js file type
+```javascript
+VERIF.RULES.customRuleName={
+  control:(data)=>{ return (data.trim()=='Custom :D')},
+  errorText:'A custom error message'
+}
+```
+to use :
+```html
+<input type="text" data-verif="customRuleName">
+```
+
+## Verify on button click
+
+some html :
+```html
+<input type="text" data-verif-group="groupName" data-verif="required|text|min_l:6|max_l:20">
+<input type="email" data-verif-group="groupName" data-verif="required|email">
+<input id="btn_verify" type="button">
+```
+
+```javascript
+let btn = document.getElementById('btn_verify');
+btn.addEventListener('click', ()=>{
+  let isOk=VERIF.verifGroup('groupName');
+  if(isOK){
+    console.log('all inputs are ok');
+  }else{
+    console.log('Verification ERROR');
+    return;
+  }
+}
+```
---
+++ verif.js
@@ -0,0 +1,157 @@
+const VERIF={
+  init:()=>{
+    let elems=document.querySelectorAll('[data-verif]');
+    console.log(elems);
+    elems.forEach((item)=>{
+      item.addEventListener('blur', (ev)=>{
+        VERIF.verif(item);
+      });
+    });
+  },
+  verif:(me)=>{
+    let rules=me.dataset.verif.split('|');
+    let data=me.value;
+
+    for(let i=0; i<rules.length ; i++){
+      let sep=rules[i].split(':');
+      let rule=sep[0];
+      let arg=(sep.length>1)?sep[1]:null;
+      let before = me.previousElementSibling;
+      let parent = me.parentElement;
+      if(typeof VERIF.RULES[rule]=='undefined'){
+        console.error('RULE "{0}" does not exits in VERIF.RULES'.replace(/\{0\}/,rule));
+        continue;
+      }
+      if(!VERIF.RULES[rule].control(data,arg)){
+        if(me.className.indexOf('verif_error')<0)
+          me.className+=' verif_error';
+        let errtxt=VERIF.RULES[rule].errorText;
+        let before_build=document.createElement('div');
+        before_build.className='verif_message_error';
+
+        if(before == null) before = parent.insertBefore(before_build,me);
+        if(before.className.indexOf('verif_message_error')<0){
+          before = parent.insertBefore(before_build,me);
+        }
+        before.textContent=errtxt.replace(/\{0\}/,arg);
+
+        return false;
+      }else if(me.className.indexOf('verif_error')>=0){
+        me.className=me.className.replace('verif_error','')
+        if(before != null){
+          if(before.className.indexOf('verif_message_error')>=0){
+            parent.removeChild(before);
+          }
+        }
+      }
+    }
+    return true;
+  },
+  verifGroup:(groupId)=>{
+    let isOk=true;
+    let elems=document.querySelectorAll('[data-verif-group="{0}"] [data-verif]'
+                                        .replace(/\{0\}/,groupId));
+    for(let item in elems){
+      if(!VERIF.verif(item)) isOk=false;
+    }
+
+    return isOk;
+  },
+  RULES:{
+    required:{
+      control:(data)=>{return (data.trim().length>0);},
+      errorText:'Ce champs est requis'
+    },
+    email:{
+      control:(data)=>{return /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,12}$/i.test(data.trim());},
+      errorText:'Veuillez entrer un email correcte'
+    },
+    alphanumerique:{
+      control:(data)=>{return /^\w+$/.test(data.trim());},
+      errorText:'Ce champs dois être alphanumerique sans espace [A-Za-z0-9_]'
+    },
+    text:{
+      control:(data)=>{return /^[\w\ ]+$/.test(data.trim());},
+      errorText:'Ce champs dois être alphanumerique [A-Za-z0-9_ ]'
+    },
+    int:{
+      control:(data)=>{return /^\d+$/.test(data.trim());},
+      errorText:'Ce champs dois être nombre entier'
+    },
+    double:{
+      control:(data)=>{return /^\d+\.?\d*$/.test(data.trim());},
+      errorText:'Ce champs dois être nombre'
+    },
+    date:{
+      control:(data)=>{return /^\d{2}\.\d{2}\.\d{4}$/.test(data.trim());},
+      errorText:'Ce champs dois être une date [jj.mm.aaaa]'
+    },
+    time:{
+      control:(data)=>{return /^\d{2}:\d{2}$/.test(data.trim());},
+      errorText:'Ce champs dois être une heure [HH:MM]'
+    },
+    date_time:{
+      control:(data)=>{return /^\d{2}\.\d{2}\.\d{4}\ \d{2}:\d{2}$/.test(data.trim());},
+      errorText:'Ce champs dois être une date et heure [jj.mm.aaaa HH:MM]'
+    },
+    max_l:{
+      control:(data,max)=>{return (data.trim().length<=max);},
+      errorText:'Ce champs dois avoir moins de {0} caractères.'
+    },
+    min_l:{
+      control:(data,min)=>{return (data.trim().length>=min);},
+      errorText:'Ce champs dois avoir plus de {0} caractères.'
+    },
+    max:{
+      control:(data,max)=>{return (0+data.trim()<=max);},
+      errorText:'Doit être inférieur ou égale à {0}'
+    },
+    min:{
+      control:(data,min)=>{return (0+data.trim()>=min);},
+      errorText:'Doit être supérieur ou égale à {0}'
+    },
+    date_past:{
+      control:(data)=>{
+        let ds=data.trim().split('.');
+        let now=new Date();
+        let date=new Date(ds[2],ds[1],ds[0]);
+        return (now.getTime()-date.getTime()>0);
+      },
+      errorText:'La date doit faire partie du passé'
+    },
+    date_futur:{
+      control:(data)=>{
+        let ds=data.trim().split('.');
+        let now=new Date();
+        let date=new Date(ds[2],ds[1],ds[0]);
+        return (now.getTime()-date.getTime()<0);
+      },
+      errorText:'La date doit faire partie du futur'
+    },
+    date_more_diff:{
+      control:(data,diff)=>{
+        let ds=data.trim().split('.');
+        let now=new Date();
+        let date=new Date(ds[2],ds[1],ds[0]);
+        let ddiff=new Date(Math.abs(now.getTime()-date.getTime()));
+        let mdiff=(ddiff.getFullYear()-1970)*12+ddiff.getMonth();
+        return (mdiff>=diff);
+      },
+      errorText:'La date doit avoir une différence d\'au moins {0} mois'
+    },
+    date_less_diff:{
+      control:(data,diff)=>{
+        let ds=data.trim().split('.');
+        let now=new Date();
+        let date=new Date(ds[2],ds[1],ds[0]);
+        let ddiff=new Date(Math.abs(now.getTime()-date.getTime()));
+        let mdiff=(ddiff.getFullYear()-1970)*12+ddiff.getMonth();
+        return (mdiff<diff);
+      },
+      errorText:'La date doit avoir une différence inférieure à {0} mois'
+    },
+  }
+}
+window.addEventListener('load', function(e) {
+  VERIF.init()
+});
