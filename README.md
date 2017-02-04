5025ea4813ecf733ccb02e35d6623e7bd34fc6d6
Esteban Sotillo <westixy@gmail.com>
2017-02-04 02:11:01 +0100

    Controls can be async (with ajax for ex.)

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # VerifJS
 
-This script is used to verify field easily
+This script is used to verify field easily. Also works asynchronously.(ex: with ajax on the control)
 
 ## Getting stated
 
-1. include the script on the html `<script src="verif.js" ></script>`
+1. include the script on the html `<script src="verif.js" ></script>` (before fields that you want to verify)
 2. add a `data-verif` attribute to your inputs
 ```html
 <input type="text" data-verif="required|text|min_l:6|max_l:20">
@@ -42,27 +42,60 @@ This script is used to verify field easily
 
 ## Add a custom rule
 
-in an other js file type
+in an other js file, type
 ```javascript
 VERIF.addRule(
   'customRuleName',
-  (data)=>{ return (data.trim()=='Custom :D')},
-  'A custom error message'
+  // arg can be null
+  (result,data,arg)=>{ result(data.trim()==arg)},
+  'A custom error message {0}',
+  // not required
+  (arg)=>{return arg+' some text'}
 );
 ```
 or
 ```javascript
 VERIF.RULES.customRuleName={
-  control:(data)=>{ return (data.trim()=='Custom :D')},
-  errorText:'A custom error message'
+  // arg can be null
+  control:(result,data,arg)=>{ result(data.trim()==arg)},
+  errorText:'A custom error message',
+  // not required
+  argToError:(arg)=>{return arg+' some text'}
 }
 ```
 to use :
 ```html
-<input type="text" data-verif="customRuleName">
+                          <!-- arg not required -->
+<input type="text" data-verif="customRuleName:arg">
 ```
 
 ## Verify on button click <small>and submit</small>
+
+### For a form
+
+some html :
+```html
+<form name="myForm" method="post" action="">
+  <input type="text" data-verif="required|text|min_l:6|max_l:20">
+  <input type="email" data-verif="required|email">
+  <input id="btn_verify" type="button">
+</form>
+```
+and some js :
+```javascript
+let btn=document.getElementById('btn_verify');
+VERIF.onClickSubmitAfterVerifForm(btn,'myForm');
+```
+or
+```javascript
+document.querySelector('#btn_verify').addEventListener('click', ()=>{
+  VERIF.verifForm('myForm',(isOk)=>{
+    if(isOk) document.forms["myForm"].submit();
+  });
+});
+```
+
+### For a group
 
 some html :
 ```html
@@ -75,12 +108,48 @@ some html :
 and some js :
 ```javascript
 let btn=document.getElementById('btn_verify');
-VERIF.verifOnCLick(btn,'myForm','groupName');
+VERIF.onClickSubmitAfterVerifGroup(btn,'myForm','groupName');
 ```
 or
 ```javascript
 document.querySelector('#btn_verify').addEventListener('click', ()=>{
-  if(VERIF.verifGroup('groupName'))
-    document.forms["myForm"].submit();
+  VERIF.verifGroup('groupName',(isOk)=>{
+    if(isOk) document.forms["myForm"].submit();
+  });
 });
+```
+
+## Verify some elements
+
+some html :
+```html
+<form name="myForm" method="post" action="">
+  <input type="text" data-verif-group="groupName" data-verif="required|text|min_l:6|max_l:20">
+  <input type="email" data-verif-group="groupName" data-verif="required|email">
+  <input id="btn_verify" type="button">
+</form>
+```
+some js for an array of elements :
+```javascript
+let elems=document.querySelectorAll('form input[data-verif]');
+VERIF.verifElems(elems,(isOk)=>{if(isOK) console.log('OK');});
+```
+some js for a form :
+```javascript
+VERIF.verifForm('myForm',(isOk)=>{if(isOK) console.log('OK');});
+```
+some js for a group :
+```javascript
+VERIF.verifForm('groupName',(isOk)=>{if(isOK) console.log('OK');});
+```
+
+## Verify one field
+
+some html :
+```html
+<input id="myField" type="text" data-verif="required|phone">
+```
+and some js:
+```javascript
+VERIF.verif(document.getElementById('myField'),(res)=>{if(res) console.log('OK');});
 ```
--- verif.js
+++ verif.js
@@ -1,4 +1,5 @@
 const VERIF={
+  version:'1.0.0',
   init:()=>{
     let elems=document.querySelectorAll('[data-verif]');
     elems.forEach((item)=>{
@@ -7,166 +8,219 @@ const VERIF={
       });
     });
   },
-  verif:(me)=>{
+  verif:(me,endCb)=>{
+    endCb = endCb||function(){};
     let rules=me.dataset.verif.split('|');
     let data=me.value;
+    let before = me.previousElementSibling;
+    let parent = me.parentElement;
+    var result=true;
+    var resnbr=0;
 
     for(let i=0; i<rules.length ; i++){
       let sep=rules[i].split(':');
       let rule=sep[0];
       let arg=(sep.length>1)?sep[1]:null;
-      let before = me.previousElementSibling;
-      let parent = me.parentElement;
+
       if(typeof VERIF.RULES[rule]=='undefined'){
         console.error('RULE "{0}" does not exits in VERIF.RULES'.replace(/\{0\}/,rule));
         continue;
       }
-      if(!VERIF.RULES[rule].control(data,arg)){
-        if(me.className.indexOf('verif_error')<0)
-          me.className+=' verif_error';
-        let errtxt=VERIF.RULES[rule].errorText;
-        let before_build=document.createElement('div');
-        before_build.className='verif_message_error';
+      var cbResult=(res)=>{
+        if(result){
+          resnbr++;
+          if(!res){
+            result=false;
+            if(me.className.indexOf('verif_error')<0)
+              me.className+=' verif_error';
+            let errtxt=VERIF.RULES[rule].errorText;
+            let before_build=document.createElement('div');
+            before_build.className='verif_message_error';
 
-        if(before == null) before = parent.insertBefore(before_build,me);
-        if(before.className.indexOf('verif_message_error')<0){
-          before = parent.insertBefore(before_build,me);
-        }
-        before.textContent=errtxt.replace(/\{0\}/,arg);
+            if(before == null) before = parent.insertBefore(before_build,me);
+            if(before.className.indexOf('verif_message_error')<0){
+              before = parent.insertBefore(before_build,me);
+            }
 
-        return false;
-      }else if(me.className.indexOf('verif_error')>=0){
-        me.className=me.className.replace('verif_error','')
-        if(before != null){
-          if(before.className.indexOf('verif_message_error')>=0){
-            parent.removeChild(before);
+            let errArg=VERIF.RULES[rule].argToError||arg;
+            before.textContent=errtxt.replace(/\{0\}/,errArg);
           }
         }
-      }
+        if(result && resnbr==rules.length){
+          me.className=me.className.replace('verif_error','')
+          if(before != null){
+            if(before.className.indexOf('verif_message_error')>=0){
+              parent.removeChild(before);
+            }
+          }
+          endCb(result);
+        }
+      };
+      VERIF.RULES[rule].control(cbResult,data,arg);
     }
-    return true;
   },
-  verifGroup:(groupId)=>{
-    let isOk=true;
+  verifElems:(elems,after)=>{
+    var isOk=true;
+    var resnbr=0;
+    for(let i=0;i<elems.length;i++){
+      var onOneEnd=(res)=>{
+        resnbr++;
+        if(isOk && !res){
+          isOk==false
+        }
+        if(resnbr==elems.length){
+          after(isOk);
+        }
+      };
+      VERIF.verif(elems[i],onOneEnd);
+    }
+  },
+  verifGroup:(groupId,after)=>{
     let elems=document.querySelectorAll('[data-verif-group="{0}"][data-verif]'
                                         .replace(/\{0\}/,groupId));
-    for(let i=0;i<elems.length;i++){
-      if(!VERIF.verif(elems[i])) isOk=false;
-    }
-
-    return isOk;
+    VERIF.verifElems(elems,after);
   },
-  verifOnCLick:(elem,formName,formGroup)=>{
+  verifForm:(formName,after)=>{
+    let elems=document.querySelectorAll('form[name="{0}"] [data-verif]'
+                                        .replace(/\{0\}/,formName));
+    VERIF.verifElems(elems,after);
+  },
+  verifContained:(elemQuery,after)=>{
+    let elems=document.querySelectorAll('{0} [data-verif]'
+                                        .replace(/\{0\}/,elemQuery));
+    VERIF.verifElems(elems,after);
+  },
+  onClickSubmitAfterVerifGroup:(elem,formName,formGroup)=>{
     elem.addEventListener('click', (e)=>{
-      if(VERIF.verifGroup(formGroup))
-        document.forms[formName].submit();
+      VERIF.verifGroup(formGroup,(isOk)=>{
+        if(isOk) document.forms[formName].submit();
+      });
     });
   },
-  addRule:(name,control,errorText)=>{
+  onClickSubmitAfterVerifForm:(elem,formName)=>{
+    elem.addEventListener('click', (e)=>{
+      VERIF.verifForm(formName,(isOk)=>{
+        if(isOk) document.forms[formName].submit();
+      });
+    });
+  },
+  addRule:(name,control,errorText,argToError)=>{
     VERIF.RULES[name]={};
     VERIF.RULES[name].control=control;
     VERIF.RULES[name].errorText=errorText;
+    if(argToError!='undefined')
+      VERIF.RULES[name].argToError=argToError;
   },
   RULES:{
     required:{
-      control:(data)=>{return (data.trim().length>0);},
+      control:(res,data)=>{res(data.trim().length>0);},
       errorText:'Ce champs est requis'
     },
     alphanumerique:{
-      control:(data)=>{return /^\w+$/.test(data.trim());},
+      control:(res,data)=>{res(/^\w+$/.test(data.trim()));},
       errorText:'Ce champs dois être alphanumerique sans espace [A-Za-z0-9_]'
     },
     text:{
-      control:(data)=>{return /^[\w\ ]+$/.test(data.trim());},
+      control:(res,data)=>{res(/^[\w\ ]+$/.test(data.trim()));},
       errorText:'Ce champs dois être alphanumerique [A-Za-z0-9_ ]'
     },
     int:{
-      control:(data)=>{return /^\d+$/.test(data.trim());},
+      control:(res,data)=>{res(/^\d+$/.test(data.trim()));},
       errorText:'Ce champs dois être nombre entier'
     },
     double:{
-      control:(data)=>{return /^\d+\.?\d*$/.test(data.trim());},
+      control:(res,data)=>{res(/^\d+\.?\d*$/.test(data.trim()));},
       errorText:'Ce champs dois être nombre'
     },
     date:{
-      control:(data)=>{return /^\d{2}\.\d{2}\.\d{4}$/.test(data.trim());},
+      control:(res,data)=>{res(/^\d{2}\.\d{2}\.\d{4}$/.test(data.trim()));},
       errorText:'Ce champs dois être une date [jj.mm.aaaa]'
     },
     time:{
-      control:(data)=>{return /^\d{2}:\d{2}$/.test(data.trim());},
+      control:(res,data)=>{res(/^\d{2}:\d{2}$/.test(data.trim()));},
       errorText:'Ce champs dois être une heure [HH:MM]'
     },
     date_time:{
-      control:(data)=>{return /^\d{2}\.\d{2}\.\d{4}\ \d{2}:\d{2}$/.test(data.trim());},
+      control:(res,data)=>{res(/^\d{2}\.\d{2}\.\d{4}\ \d{2}:\d{2}$/.test(data.trim()));},
       errorText:'Ce champs dois être une date et heure [jj.mm.aaaa HH:MM]'
     },
     max_l:{
-      control:(data,max)=>{return (data.trim().length<=max);},
+      control:(res,data,max)=>{res(data.trim().length<=max);},
       errorText:'Ce champs dois avoir moins de {0} caractères.'
     },
     min_l:{
-      control:(data,min)=>{return (data.trim().length>=min);},
+      control:(res,data,min)=>{res(data.trim().length>=min);},
       errorText:'Ce champs dois avoir plus de {0} caractères.'
     },
     max:{
-      control:(data,max)=>{return (data.trim()<=max);},
+      control:(res,data,max)=>{res(data.trim()<=max);},
       errorText:'Doit être inférieur ou égale à {0}'
     },
     min:{
-      control:(data,min)=>{return (data.trim()>=min);},
+      control:(res,data,min)=>{res(data.trim()>=min);},
       errorText:'Doit être supérieur ou égale à {0}'
     },
     email:{
-      control:(data)=>{return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                              .test(data.trim());},
+      control:(res,data)=>{res(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                              .test(data.trim()));},
       errorText:'Veuillez entrer un email correcte'
     },
     phone:{
-      control:(data)=>{
-        return /^(((\+|00)\d{2,3})|0)([./ -]?\d){9}$/
-               .test(data.trim());
+      control:(res,data)=>{
+        res(/^(((\+|00)\d{2,3})|0)([./ -]?\d){9}$/
+               .test(data.trim()));
       },
       errorText:'Ce champs dois être un numéro de téléphone valide'
     },
     date_past:{
-      control:(data)=>{
+      control:(res,data)=>{
         let ds=data.trim().split('.');
         let now=new Date();
         let date=new Date(ds[2],ds[1],ds[0]);
-        return (now.getTime()-date.getTime()>0);
+        res(now.getTime()-date.getTime()>0);
       },
       errorText:'La date doit faire partie du passé'
     },
     date_futur:{
-      control:(data)=>{
+      control:(res,data)=>{
         let ds=data.trim().split('.');
         let now=new Date();
         let date=new Date(ds[2],ds[1],ds[0]);
-        return (now.getTime()-date.getTime()<0);
+        res(now.getTime()-date.getTime()<0);
       },
       errorText:'La date doit faire partie du futur'
     },
     date_more_diff:{
-      control:(data,diff)=>{
+      control:(res,data,diff)=>{
         let ds=data.trim().split('.');
         let now=new Date();
         let date=new Date(ds[2],ds[1],ds[0]);
         let ddiff=new Date(Math.abs(now.getTime()-date.getTime()));
         let mdiff=(ddiff.getFullYear()-1970)*12+ddiff.getMonth();
-        return (mdiff>=diff);
+        res(mdiff>=diff);
       },
-      errorText:'La date doit avoir une différence d\'au moins {0} mois'
+      errorText:'La date doit avoir une différence d\'au moins {0}',
+      argToError:(arg)=>{
+        let y = Math.ceil(arg/12);
+        let m = arg%12;
+        return (((y>0)? y+((y==1)?' an':' ans'):'')+((m>0)? ' et ' : '' + ((m>0)? m+' mois':'')));
+      }
     },
     date_less_diff:{
-      control:(data,diff)=>{
+      control:(res,data,diff)=>{
         let ds=data.trim().split('.');
         let now=new Date();
         let date=new Date(ds[2],ds[1],ds[0]);
         let ddiff=new Date(Math.abs(now.getTime()-date.getTime()));
         let mdiff=(ddiff.getFullYear()-1970)*12+ddiff.getMonth();
-        return (mdiff<diff);
+        res(mdiff<diff);
       },
-      errorText:'La date doit avoir une différence inférieure à {0} mois'
+      errorText:'La date doit avoir une différence inférieure à {0}',
+      argToError:(arg)=>{
+        let y = Math.ceil(arg/12);
+        let m = arg%12;
+        return (((y>0)? y+((y==1)?' an':' ans'):'')+((m>0)? ' et ' : '' + ((m>0)? m+' mois':'')));
+      }
     },
   }
 }
